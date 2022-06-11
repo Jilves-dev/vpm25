ea1637e94bb42a1ecc5f52fead293ff2a3f5f7a7
jukkailveskorpi <jukka.ilveskorpi@edu.sasky.fi>
2022-06-11 13:19:38 +0300

    item props

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -6,18 +6,16 @@ function Item(props) {
     return(
         <div className={styles.item}>
             <div className={styles.item_data}>
-            <div className={styles.item_pv}>pv</div>
-            <div className={styles.item_apip}>apip</div>
-            <div className={styles.item_klo}>klo</div>
-            <div className={styles.item_yp}>yp</div>
-            <div className={styles.item_ap}>ap</div>
-            <div className={styles.item_syke}>syke</div>
-            <div className={styles.item_lisätieto}>lisätieto</div>
+            <div className={styles.item_pv}>{props.data.pv}</div>
+            <div className={styles.item_apip}>{props.data.apip}</div>
+            <div className={styles.item_klo}>{props.data.klo}</div>
+            <div className={styles.item_yp}>Yläpaine: {props.data.yp}</div>
+            <div className={styles.item_ap}>Alapaine: {props.data.ap}</div>
+            <div className={styles.item_syke}>Syke: {props.data.syke}</div>
+            <div className={styles.item_lisätieto}>Vointi: {props.data.lisätieto}</div>
             </div>
             <div className={styles.item_edit}>
-              
                 <MdNavigateNext />
-       
             </div>
         </div>
     )
---
+++ src/testdata.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        id: 1,
+        pv: "2020-11-19",
+        apip: "aamu",
+        klo: "13:01:36",
+        yp: 65,
+        ap: 57,
+        syke: 76,
+        lisätieto: "ok" 
+    },
+    {
+        id: 2,
+        pv: "2020-12-19",
+        apip: "aamu",
+        klo: "10:01:36",
+        yp: 63,
+        ap: 52,
+        syke: 77,
+        lisätieto: "ok" 
+    },
+    {
+        id: 3,
+        pv: "2020-12-25",
+        apip: "ilta",
+        klo: "23:01:36",
+        yp: 61,
+        ap: 54,
+        syke: 70,
+        lisätieto: "ok" 
+    },
+];
+
+export default data;
+
+/*<script>
+const d = new Date();
+let text = d.toTimeString();
+document.getElementById("demo").innerHTML = text;
+</script>*/
