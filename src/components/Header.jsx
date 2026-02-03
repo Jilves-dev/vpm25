59b35ffd5f4d628f12ce930cf52763dec2bde834
OivaHuuskonen <156306030+OivaHuuskonen@users.noreply.github.com>
2026-02-03 23:30:51 +0200

    ry54745

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="bg-red-600 text-white h-16 text-[1.75rem] flex justify-center items-center font-InstrumentSerif">
+    <div className="bg-red-600 text-white h-16 text-[1.75rem] flex justify-center items-center font-comic">
       Verenpaine mittaus
     </div>
   );
